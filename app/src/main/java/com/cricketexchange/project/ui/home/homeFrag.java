3c4906411adfc1c680178abbcd6b155a58581812
rachit singh <singhrachit569@gmail.com>
2021-05-08 12:44:37 +0530

    V 1.0.0 -beta

---
+++ app/src/main/java/com/cricketexchange/project/ui/home/homeFrag.java
@@ -0,0 +1,20 @@
+package com.cricketexchange.project.ui.home;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.cricketexchange.project.R;
+
+public class homeFrag extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+}
