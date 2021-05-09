158f6aa546c3a9e1f125e437915bf1d8a770ea5c
creator-jay <62167994+creator-jay@users.noreply.github.com>
2021-05-09 16:07:36 +0530

    date:9-5-2021(1)
    news section added  with recyclerview of content and ads successfully

---
+++ app/src/main/java/com/cricketexchange/project/Adapter/Recyclerview/NewsRecyclerAdapter.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (C) 2017 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cricketexchange.project.Adapter.Recyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cricketexchange.project.Models.NewsModel;
+import com.cricketexchange.project.R;
+import com.google.android.gms.ads.formats.NativeAd;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
+import com.google.android.gms.ads.formats.UnifiedNativeAdView;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * The {@link NewsRecyclerAdapter} class.
+ * <p>The adapter provides access to the items in the {@link MenuItemViewHolder}
+ */
+public class NewsRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    // A menu item view type.
+    private static final int MENU_ITEM_VIEW_TYPE = 0;
+
+    private static final int UNIFIED_NATIVE_AD_VIEW_TYPE = 1;
+
+    // An Activity's Context.
+    private final Context mContext;
+
+    // The list of Native ads and menu items.
+    private final List<Object> mRecyclerViewItems;
+
+    public NewsRecyclerAdapter(Context context, List<Object> recyclerViewItems) {
+        this.mContext = context;
+        this.mRecyclerViewItems = recyclerViewItems;
+    }
+
+    /**
+     * The {@link MenuItemViewHolder} class.
+     * Provides a reference to each view in the menu item view.
+     */
+    public class MenuItemViewHolder extends RecyclerView.ViewHolder {
+        TextView maintitle, secondarytitle, time;
+        ImageView poster;
+        CardView card;
+
+        MenuItemViewHolder(View v) {
+            super(v);
+            maintitle = v.findViewById(R.id.maintitle);
+            secondarytitle = v.findViewById(R.id.secondarytitle);
+            time = v.findViewById(R.id.time);
+            poster = v.findViewById(R.id.poster);
+            card = v.findViewById(R.id.card);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecyclerViewItems.size();
+    }
+
+    /**
+     * Determines the view type for the given position.
+     */
+    @Override
+    public int getItemViewType(int position) {
+
+        Object recyclerViewItem = mRecyclerViewItems.get(position);
+        if (recyclerViewItem instanceof UnifiedNativeAd) {
+            return UNIFIED_NATIVE_AD_VIEW_TYPE;
+        }
+        return MENU_ITEM_VIEW_TYPE;
+    }
+
+    /**
+     * Creates a new view for a menu item view or a Native ad view
+     * based on the viewType. This method is invoked by the layout manager.
+     */
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        switch (viewType) {
+            case UNIFIED_NATIVE_AD_VIEW_TYPE:
+                View unifiedNativeLayoutView = LayoutInflater.from(
+                        viewGroup.getContext()).inflate(R.layout.ad_unified,
+                        viewGroup, false);
+                return new UnifiedNativeAdViewHolder(unifiedNativeLayoutView);
+            case MENU_ITEM_VIEW_TYPE:
+                // Fall through.
+            default:
+                View menuItemLayoutView = LayoutInflater.from(viewGroup.getContext()).inflate(
+                        R.layout.card_news_rv, viewGroup, false);
+                return new MenuItemViewHolder(menuItemLayoutView);
+        }
+    }
+
+    /**
+     * Replaces the content in the views that make up the menu item view and the
+     * Native ad view. This method is invoked by the layout manager.
+     */
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int viewType = getItemViewType(position);
+        switch (viewType) {
+            case UNIFIED_NATIVE_AD_VIEW_TYPE:
+                UnifiedNativeAd nativeAd = (UnifiedNativeAd) mRecyclerViewItems.get(position);
+                populateNativeAdView(nativeAd, ((UnifiedNativeAdViewHolder) holder).getAdView());
+                break;
+            case MENU_ITEM_VIEW_TYPE:
+                MenuItemViewHolder menuItemHolder = (MenuItemViewHolder) holder;
+                NewsModel menuItem = (NewsModel) mRecyclerViewItems.get(position);//content holder
+                if (menuItem!=null){
+                    (menuItemHolder).maintitle.setText(menuItem.getMaintitle());
+                    (menuItemHolder).secondarytitle.setText(menuItem.getSecondarytitle());
+                    Picasso.get().load(mContext.getString(R.string.sampleimageurl)).into((menuItemHolder).poster);
+                    (menuItemHolder).time.setText(menuItem.getTime());
+                    menuItemHolder.card.setOnClickListener(view -> {
+                        Toast.makeText(mContext, "item clicked : " + position, Toast.LENGTH_SHORT).show();
+                    });
+                }
+
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    private void populateNativeAdView(UnifiedNativeAd nativeAd,
+                                      UnifiedNativeAdView adView) {
+        // Some assets are guaranteed to be in every UnifiedNativeAd.
+        ((TextView) adView.getHeadlineView()).setText(nativeAd.getHeadline());
+        ((TextView) adView.getBodyView()).setText(nativeAd.getBody());
+        ((Button) adView.getCallToActionView()).setText(nativeAd.getCallToAction());
+
+        // These assets aren't guaranteed to be in every UnifiedNativeAd, so it's important to
+        // check before trying to display them.
+        NativeAd.Image icon = nativeAd.getIcon();
+
+        if (icon == null) {
+            adView.getIconView().setVisibility(View.INVISIBLE);
+        } else {
+            ((ImageView) adView.getIconView()).setImageDrawable(icon.getDrawable());
+            adView.getIconView().setVisibility(View.VISIBLE);
+        }
+
+        if (nativeAd.getPrice() == null) {
+            adView.getPriceView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getPriceView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getPriceView()).setText(nativeAd.getPrice());
+        }
+
+        if (nativeAd.getStore() == null) {
+            adView.getStoreView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getStoreView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getStoreView()).setText(nativeAd.getStore());
+        }
+
+        if (nativeAd.getStarRating() == null) {
+            adView.getStarRatingView().setVisibility(View.INVISIBLE);
+        } else {
+            ((RatingBar) adView.getStarRatingView())
+                    .setRating(nativeAd.getStarRating().floatValue());
+            adView.getStarRatingView().setVisibility(View.VISIBLE);
+        }
+
+        if (nativeAd.getAdvertiser() == null) {
+            adView.getAdvertiserView().setVisibility(View.INVISIBLE);
+        } else {
+            ((TextView) adView.getAdvertiserView()).setText(nativeAd.getAdvertiser());
+            adView.getAdvertiserView().setVisibility(View.VISIBLE);
+        }
+
+        // Assign native ad object to the native view.
+        adView.setNativeAd(nativeAd);
+    }
+}
--- app/src/main/java/com/cricketexchange/project/ui/News/newsFrag.java
+++ app/src/main/java/com/cricketexchange/project/ui/News/newsFrag.java
@@ -1,20 +1,155 @@
 package com.cricketexchange.project.ui.News;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.cricketexchange.project.Adapter.Recyclerview.NewsRecyclerAdapter;
+import com.cricketexchange.project.Models.NewsModel;
 import com.cricketexchange.project.R;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class newsFrag extends Fragment {
+
+    // The number of native ads to load.
+    public static final int NUMBER_OF_ADS = 5;
+    public static final int ADS_PER_POST = 4;
+
+    // The AdLoader used to load ads.
+    private AdLoader adLoader;
+
+    // List of MenuItems and native ads that populate the RecyclerView.
+    private List<Object> mRecyclerViewItems = new ArrayList<>();
+
+    // List of native ads that have been successfully loaded.
+    private List<UnifiedNativeAd> mNativeAds = new ArrayList<>();
+    NewsRecyclerAdapter adapter;
+    RecyclerView mRecyclerView;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.news_fragment, container, false);
+        View view = inflater.inflate(R.layout.news_fragment, container, false);
+        //init the ads
+        MobileAds.initialize(getActivity(), initializationStatus -> {
+        });
+
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView.
+        mRecyclerView.setHasFixedSize(true);
+
+        // Specify a linear layout manager.
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        // Specify an adapter.
+        adapter = new NewsRecyclerAdapter(getActivity(), mRecyclerViewItems);
+        mRecyclerView.setAdapter(adapter);
+        MobileAds.initialize(getActivity(), initializationStatus -> {
+        });
+        loadNativeAds();
+        addMenuItemsFromJson();
+        return view;
     }
+
+    int size = 0;
+
+    private void addMenuItemsFromJson() {
+        size = 60 + NUMBER_OF_ADS;
+
+        for (int i = 0; i < size; i++) {
+//            if (i != 0 && i % ADS_PER_POST == 0) {
+//                mRecyclerViewItems.add(null);
+//            } else {
+//
+//            }
+            NewsModel newsModel = new NewsModel("" + i, this.getString(R.string.sample_title), this.getString(R.string.sample_title), "2 hours ago", "https://homepages.cae.wisc.edu/~ece533/images/arctichare.png");
+            mRecyclerViewItems.add(newsModel);
+
+
+        }
+        adapter.notifyDataSetChanged();
+
+    }
+
+    private void loadNativeAds() {
+
+        AdLoader.Builder builder = new AdLoader.Builder(getActivity(), getString(R.string.admob_nativ_ads_id1));
+        adLoader = builder.forUnifiedNativeAd(
+                new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
+                    @Override
+                    public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
+                        // A native ad loaded successfully, check if the ad loader has finished loading
+                        // and if so, insert the ads into the list.
+                        mNativeAds.add(unifiedNativeAd);
+                        if (!adLoader.isLoading()) {
+                            insertAdsInMenuItems();
+                        }
+                    }
+                }).withAdListener(
+                new AdListener() {
+                    @Override
+                    public void onAdFailedToLoad(int errorCode) {
+                        // A native ad failed to load, check if the ad loader has finished loading
+                        // and if so, insert the ads into the list.
+                        Log.e("MainActivity", "The previous native ad failed to load. Attempting to"
+                                + " load another.");
+                        if (!adLoader.isLoading()) {
+                            insertAdsInMenuItems();
+                        }
+                    }
+                }).build();
+
+        // Load the Native ads.
+        adLoader.loadAds(new AdRequest.Builder().build(), NUMBER_OF_ADS);
+    }
+
+
+    private void insertAdsInMenuItems() {
+        if (mNativeAds.size() <= 0) {
+            return;
+        }
+
+        int offset = 5;
+        int index = 0;
+        int indx = 0;
+        for (int i = 0; i < mRecyclerViewItems.size(); i++) {
+            if (indx > mNativeAds.size() - 1) {
+                indx = 0;
+            }
+            if (index > mRecyclerViewItems.size()) {
+                break;
+            }
+            if (index != 0) {
+                UnifiedNativeAd ad = mNativeAds.get(indx);
+                mRecyclerViewItems.add(index, ad);
+            }
+
+            index = index + offset;
+
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    // public List<Object> getRecyclerViewItems() {
+    //     return mRecyclerViewItems;
+    //  }
 
 }
