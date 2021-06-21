d44b24af80f4e77b87211bfc2349bfcb4df41b86
rachit singh <singhrachit569@gmail.com>
2021-06-21 13:12:24 +0530

    update

--- app/src/main/java/com/cricketexchange/project/Activity/SplashScreen.java
+++ app/src/main/java/com/cricketexchange/project/Activity/SplashScreen.java
@@ -24,7 +24,7 @@ public class SplashScreen extends AppCompatActivity {
             public void run() {
                 refresh();
             }
-        },5500);
+        },4500);
 
     }
 
@@ -33,8 +33,10 @@ public class SplashScreen extends AppCompatActivity {
         boolean connection=isNetworkAvailable();
         if(connection==false){
             startActivity(new Intent(SplashScreen.this, NetworkFailureActivity.class));
+            finish();
         }else{
             startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            finish();
         }
     }
     public boolean isNetworkAvailable(){
